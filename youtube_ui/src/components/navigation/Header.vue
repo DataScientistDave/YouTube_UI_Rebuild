4b0de67f9d5a8650a5ecef1c9beb276b46ba02b0
Nisarg Dave <extrastuff58@yahoo.com>
2021-07-30 16:56:36 +0800

    Added icons to navbar

---
+++ youtube_ui/src/components/navigation/Header.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <Navbar />
+  </div>
+</template>
+
+<script>
+  import Navbar from './Navbar.vue';
+  export default {
+    name:'Header',
+    components: {
+      Navbar,
+    }
+  };
+</script>
+
+<style lang="scss" scoped></style>
---
+++ youtube_ui/src/components/navigation/Navbar.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <v-app-bar flat class="px-5">
+      <v-app-bar-nav-icon />
+      <v-icon large class="pl-5"> mdi-youtube </v-icon>
+      <h2> YouTube </h2>
+      <v-spacer />
+      <v-btn icon> <v-icon> mdi-microphone </v-icon> </v-btn>
+      <v-icon> mdi-video-plus </v-icon>
+      <v-icon> mdi-apps </v-icon>
+      <v-icon> mdi-bell </v-icon>
+    </v-app-bar>
+  </div>
+</template>
+
+<script>
+  export default {
+    name:'Navbar',
+  };
+  
+</script>
+
+<style lang="scss" scoped></style>
