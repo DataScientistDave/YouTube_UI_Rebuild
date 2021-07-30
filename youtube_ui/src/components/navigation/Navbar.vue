e8420a9ffffd0f3638d6048ffbf9f4b9ec929856
Nisarg Dave <extrastuff58@yahoo.com>
2021-07-30 22:22:23 +0800

    Made NavBar a bit more responsive

--- youtube_ui/src/components/navigation/Navbar.vue
+++ youtube_ui/src/components/navigation/Navbar.vue
@@ -4,8 +4,11 @@
       <v-app-bar-nav-icon />
       <v-icon large class="ml-5"> mdi-youtube </v-icon>
       <h2>YouTube</h2>
-      <v-text-field dense outlined label="Search" class="pt-7 ml-10" />
-      <v-btn depressed color="grey darken-3" class="my-1">
+      <v-text-field v-show="$vuetify.breakpoint.md" dense outlined label="Search" class="pt-7 ml-10" />
+      <v-btn v-show="$vuetify.breakpoint.md" depressed color="grey darken-3" class="my-1">
+        <v-icon text color="grey darken-1">mdi-magnify</v-icon>
+      </v-btn>
+      <v-btn v-show="$vuetify.breakpoint.sm" depressed color="grey darken-3" class="my-1 ml-2">
         <v-icon text color="grey darken-1">mdi-magnify</v-icon>
       </v-btn>
       <IconBtn> mdi-microphone </IconBtn>
