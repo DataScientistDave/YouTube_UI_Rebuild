1e09430492b0527bd67613d8dea22afbd2e6d328
Nisarg Dave <extrastuff58@yahoo.com>
2021-08-01 23:46:17 +0800

    Created the basic sidebar with icons

--- youtube_ui/src/components/navigation/ChipGroups.vue
+++ youtube_ui/src/components/navigation/ChipGroups.vue
@@ -1,7 +1,7 @@
 <template>
   <v-card color="primary" class="pt-2">
     <v-divider class="py-1" />
-    <v-chip-group show-arrows mandatory class="pl-3 pb-1">
+    <v-chip-group show-arrows mandatory class="pb-1">
       <Chip v-for="tag in tags" :key="tag">
         {{ tag }}
       </Chip>
--- youtube_ui/src/components/navigation/Sidebar.vue
+++ youtube_ui/src/components/navigation/Sidebar.vue
@@ -4,9 +4,28 @@
     clipped
     floating
     mini-variant
+    mini-variant-width="120"
     permanent
     color="primary"
   >
+    <v-list-item>
+      <v-list-item-content>
+        <IconBtn large class="mt-4"> mdi-home </IconBtn>
+        <v-list-item-subtitle class="text-center"> Home </v-list-item-subtitle>
+        <IconBtn large class="mt-7"> mdi-compass </IconBtn>
+        <v-list-item-subtitle class="text-center mb-7">
+          Explore
+        </v-list-item-subtitle>
+        <IconBtn large> mdi-youtube-subscription </IconBtn>
+        <v-list-item-subtitle class="text-center mb-7">
+          Subscription
+        </v-list-item-subtitle>
+        <IconBtn large> mdi-plus-box-multiple </IconBtn>
+        <v-list-item-subtitle class="text-center">
+          Library
+        </v-list-item-subtitle>
+      </v-list-item-content>
+    </v-list-item>
   </v-navigation-drawer>
 </template>
 
