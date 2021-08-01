10159babc19e5912d82d70531802ac2c1d82f2db
Nisarg Dave <extrastuff58@yahoo.com>
2021-08-01 23:19:43 +0800

    Added dividers between chip groups

--- youtube_ui/src/components/navigation/ChipGroups.vue
+++ youtube_ui/src/components/navigation/ChipGroups.vue
@@ -1,10 +1,12 @@
 <template>
-  <v-card color="primary" class="py-2">
-    <v-chip-group show-arrows class="pl-3">
+  <v-card color="primary" class="pt-2">
+    <v-divider class="py-1" />
+    <v-chip-group show-arrows mandatory class="pl-3 pb-1">
       <Chip v-for="tag in tags" :key="tag">
         {{ tag }}
       </Chip>
     </v-chip-group>
+    <v-divider />
   </v-card>
 </template>
 
